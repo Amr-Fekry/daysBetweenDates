e6a47c62a8d927ede3e31342e4fc0711140aa144
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2018-12-15 18:57:56 +0200

    a- write nextDay() for simple case

--- daysBetweenDates.py
+++ daysBetweenDates.py
@@ -15,3 +15,35 @@
 ##    while date1 is before date2:
 ##        days += 1
 ##        date1 = advance to next day 
+
+## a- write nextDay(year,month,day) function to solve "advance to next day" part of the pseudo code for simple case:
+
+def nextDay(year,month,day):
+	"""takes the date of a day and returns the date of the next day (SIMPLE version that assumes all months are 30 days)"""
+
+	day += 1
+	if day > 30:
+		day = 1
+		month += 1
+		if month > 12:
+			month = 1
+			year += 1
+	return year, month, day
+
+'''
+## instructor solution:
+def nextDay(year,month,day):
+	""""""
+	if day < 30:
+		return year, month, day + 1
+	elif month < 12:
+		return year, month + 1, 1
+	else:
+		return year + 1, 1, 1
+'''
+
+## testing
+print nextDay(1992, 10, 1) # (1992, 10, 2)
+print nextDay(1999, 12, 30) # (2000, 1, 1)
+print nextDay(2013, 1, 30) # (2013, 2, 1)
+print nextDay(2012, 12, 30) # (2013, 1, 1)
